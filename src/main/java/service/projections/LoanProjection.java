85a2ed3ef4c01a840dca5b4d57ca1d8e7895b3e0
yura <yuriy.logosha@gmail.com>
2017-11-06 19:05:24 +0200

    Rename projection

--- src/main/java/service/projections/LoanProjection.java
+++ src/main/java/service/projections/LoanProjection.java
@@ -1,16 +1,20 @@
 package service.projections;
 
 import org.springframework.data.rest.core.config.Projection;
+import service.domain.Extension;
 import service.domain.Loan;
 
 import java.math.BigDecimal;
+import java.util.List;
 
-@Projection(name = "show" , types = Loan.class)
+@Projection(name = "viewLoan" , types = Loan.class)
 public interface LoanProjection {
 
     BigDecimal getAmount();
 
     int getTerm();
 
+    double getInterest();
 
+    List<Extension> getExtensions();
 }
