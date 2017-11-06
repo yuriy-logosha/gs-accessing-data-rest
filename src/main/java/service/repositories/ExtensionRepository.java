063b80cac4d2826bb131b6ce1423931e7872d4b8
yura <yuriy.logosha@gmail.com>
2017-11-06 07:04:14 +0200

    Hide delete methods for Extension

--- src/main/java/service/repositories/ExtensionRepository.java
+++ src/main/java/service/repositories/ExtensionRepository.java
@@ -3,10 +3,19 @@ package service.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 import service.domain.Extension;
+import service.domain.Loan;
 
 @RepositoryRestResource
 public interface ExtensionRepository extends CrudRepository<Extension, Long> {
 
+    @Override
+    @RestResource(exported = false)
+    public void delete(Extension t);
+
+    @Override
+    @RestResource(exported = false)
+    public void delete(Long id);
 }
